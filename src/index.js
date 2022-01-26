a995bc639229e9940db15725502fb98b356c06ba
170webnet <anderson@170web.net>
2022-01-26 23:01:07 +0000

    Challenge  done

--- src/index.js
+++ src/index.js
@@ -100,14 +100,37 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   return response.json(todo);
 });
 
-
-
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if (!todo) {
+    return response.status(404).json({error: `TODO: ${id}, not found!!` });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+  //findIndex busca a posicao do array
+  
+  if (todoIndex === -1) {
+  return response.status(404).json({ error: `TODO: ${id} nor found!!` });
+  }
+
+  user.todos.splice(todoIndex, 1);
+  //para excluir usamos SPLICE que recebe 2 argumentos (a posicao inicial, qtde que quer excluir) ex.(todoIndex, 1)
+
+  return response.status(204).json();
+
 });
 
 module.exports = app;
